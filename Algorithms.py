ad0b1f0fbbbeacdbbea51a104d2a032e71924c3e
Wummer <a.wahl.r@gmail.com>
2013-10-08 10:55:40 +0200

    Gaussian NB done - testing KNN

--- Algorithms.py
+++ Algorithms.py
@@ -1,3 +1,23 @@
 from __future__ import division
+from sklearn import datasets
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbours import KNeighborsClassifier as KNN
+from sklearn.feature_Extraction.text import CountVectorizer as Vectorizer
 
-20/3
+
+#Here we create out classifier variables
+gnb= GaussianNB()
+
+
+#Here we create our dataset variables
+bookpath = 
+iris = datasets.load_iris()
+
+
+#The call
+y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
+
+
+
+#The output
+print("Number of mislabeled points: %d" % (iris.target != y_pred).sum())
